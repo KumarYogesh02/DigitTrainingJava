1c7cb183b56a7e1421e1e10ce34ecb5f4110d34d
KumarYogesh02 <139847483+KumarYogesh02@users.noreply.github.com>
2023-07-18 12:41:12 +0530

    Update Testing_Class.java
    new change

--- src/main/java/com/digit/test/proj/p1/Testing_Class.java
+++ src/main/java/com/digit/test/proj/p1/Testing_Class.java
@@ -4,8 +4,7 @@ public class Testing_Class {
 	public static void main(String[] args) {
 		System.out.println("Hi from yogesh");
 		System.out.println("Hi from yogesh");
-		System.out.println("Hi from yogesh");
-		System.out.println("Hi from yogesh");
+		
 	}
 
 }
