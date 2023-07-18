0a18dd6445364968520fb6c81360d8f05ab96aba
Yogesh.Kumar02 <Yogesh.Kumar02@LDIGITBGL-L0231.godigit.corp>
2023-07-18 12:27:14 +0530

    This is second commit

--- src/main/java/com/digit/test/proj/p1/Testing_Class.java
+++ src/main/java/com/digit/test/proj/p1/Testing_Class.java
@@ -3,6 +3,9 @@ package com.digit.test.proj.p1;
 public class Testing_Class {
 	public static void main(String[] args) {
 		System.out.println("Hi from yogesh");
+		System.out.println("Hi from yogesh");
+		System.out.println("Hi from yogesh");
+		System.out.println("Hi from yogesh");
 	}
 
 }
